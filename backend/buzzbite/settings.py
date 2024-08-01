b1acc375f87a4177d903e6d7954501d2221a2fc7
Jaeho Cho <jaeho2025@gmail.com>
2024-08-02 02:10:20 +0900

    rm database debugging

--- backend/buzzbite/settings.py
+++ backend/buzzbite/settings.py
@@ -21,11 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-print(f"DATABASE_NAME: {env('DATABASE_NAME', default='Not Set')}")
-print(f"DATABASE_USER: {env('DATABASE_USER', default='Not Set')}")
-print(f"DATABASE_PASSWORD: {env('DATABASE_PASSWORD', default='Not Set')}")
-print(f"DATABASE_HOST: {env('DATABASE_HOST', default='Not Set')}")
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
