1e51c0ad6cf8f40746a98c50d23d5dbe57dbf729
Jaeho Cho <jaeho2025@gmail.com>
2024-08-26 14:33:44 -0400

    content api tests

--- backend/content/tests.py
+++ backend/content/tests.py
@@ -5,29 +5,45 @@ from rest_framework.test import APITestCase
 from .models import Video
 from users.models import CustomUser
 
-class PostTests(APITestCase):
+class VideoTests(APITestCase):
     def setUp(self):
-        self.user = CustomUser.objects.get_or_create(username='testuser', password='password')
+        # Creating a test user
+        self.user, created = CustomUser.objects.get_or_create(username='testuser')
+        self.user.set_password('password')
+        self.user.save()
         self.client.force_authenticate(user=self.user)
 
-    def test_create_post(self):
-        url = reverse('post-list')  # Assuming 'post-list' is the name given by the router
-        data = {'title': 'Test Video', 'content': 'Test content.', 'author': self.user.id}
-        response = self.client.post(url, data, format='json')
+    def test_create_video(self):
+        url = reverse('video-list')  # Using the router name for listing/creating videos
+        data = {
+            'videoSource': 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-videos/1.mp4',
+            'caption': 'Test Video',
+            'owner': self.user.id,  # Matching the model field
+        }
+        response = self.client.post(url, data, format='json')  # Correcting the method to post
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Video.objects.count(), 1)
-        self.assertEqual(Video.objects.get().title, 'Test Video')
+        self.assertEqual(Video.objects.get().caption, 'Test Video')
 
-    def test_list_posts(self):
-        Video.objects.create(title='Test Video', content='Test content.', author=self.user)
-        url = reverse('post-list')
+    def test_list_videos(self):
+        Video.objects.create(
+            videoSource='https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-videos/1.mp4',
+            caption='Test Video',
+            owner=self.user
+        )
+        url = reverse('video-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['caption'], 'Test Video')
 
-    def test_retrieve_post(self):
-        post = Video.objects.create(title='Test Video', content='Test content.', author=self.user)
-        url = reverse('post-detail', args=[post.id])  # Assuming 'post-detail' is the name given by the router
+    def test_retrieve_video(self):
+        video = Video.objects.create(
+            videoSource='https://notjustdev-dummy.s3.us-east-2.amazonaws.com/vertical-videos/1.mp4',
+            caption='Test Video',
+            owner=self.user
+        )
+        url = reverse('video-detail', args=[video.id])  # Correcting the reverse lookup
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], 'Test Video')
+        self.assertEqual(response.data['caption'], 'Test Video')
